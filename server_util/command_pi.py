9bd814b482a9aaee492a0d85c17b47e0513adeeb
sdivcom-dotcom <sdivcom@yandex.ru>
2024-03-09 16:58:02 +0300

    edit emulator keyboard

--- server_util/command_pi.py
+++ server_util/command_pi.py
@@ -13,99 +13,115 @@ def write_com_action(port, data):
 
 
 def response_spec_button(string):
-    if string == "key.f1":
+    if string == "Key.f1":
         data = b'1'
-        write_com_action(port_2, data)
-    elif string == "key.f2":
+        write_com_action(port_1, data)
+    elif string == "Key.f2":
         data = b'2'
-        write_com_action(port_2, data)
-    elif string == "key.f3":
+        write_com_action(port_1, data)
+    elif string == "Key.f3":
         data = b'3'
-        write_com_action(port_2, data)
-    elif string == "key.f4":
+        write_com_action(port_1, data)
+    elif string == "Key.f4":
         data = b'4'
-        write_com_action(port_2, data)
-    elif string == "key.f5":
+        write_com_action(port_1, data)
+    elif string == "Key.f5":
         data = b'5'
-        write_com_action(port_2, data)
-    elif string == "key.f6":
+        write_com_action(port_1, data)
+    elif string == "Key.f6":
         data = b'6'
-        write_com_action(port_2, data)
-    elif string == "key.f7":
+        write_com_action(port_1, data)
+    elif string == "Key.f7":
         data = b'7'
-        write_com_action(port_2, data)
-    elif string == "key.f8":
+        write_com_action(port_1, data)
+    elif string == "Key.f8":
         data = b'8'
-        write_com_action(port_2, data)
-    elif string == "key.f9":
+        write_com_action(port_1, data)
+    elif string == "Key.f9":
         data = b'9'
-        write_com_action(port_2, data)
-    elif string == "key.f10":
+        write_com_action(port_1, data)
+    elif string == "Key.f10":
         data = b'q'
-        write_com_action(port_2, data)
-    elif string == "key.f11":
+        write_com_action(port_1, data)
+    elif string == "Key.f11":
         data = b'w'
-        write_com_action(port_2, data)
-    elif string == "key.f12":
+        write_com_action(port_1, data)
+    elif string == "Key.f12":
         data = b'e'
-        write_com_action(port_2, data)
-    elif string == "key.caps":
+        write_com_action(port_1, data)
+    elif string == "Key.caps":
         data = b'a'
-        write_com_action(port_2, data)     
+        write_com_action(port_1, data)     
     elif string == "Key.print_screen":
         data = b's'
-        write_com_action(port_2, data)
+        write_com_action(port_1, data)
     elif string == "Key.scroll_lock":
         data = b'd'
-        write_com_action(port_2, data)
+        write_com_action(port_1, data)
     elif string == "Key.pause":
         data = b'f'
-        write_com_action(port_2, data)
+        write_com_action(port_1, data)
     elif string == "Key.insert":
         data = b'g'
-        write_com_action(port_2, data)  
+        write_com_action(port_1, data)  
     elif string == "Key.home":
         data = b'h'
-        write_com_action(port_2, data)
+        write_com_action(port_1, data)
     elif string == "Key.page_up":
         data = b'j'
-        write_com_action(port_2, data)
+        write_com_action(port_1, data)
     elif string == "Key.delete":
         data = b'k'
-        write_com_action(port_2, data)
+        write_com_action(port_1, data)
     elif string == "Key.end":
         data = b'l'
-        write_com_action(port_2, data)
+        write_com_action(port_1, data)
     elif string == "Key.page_down":
         data = b'z'
-        write_com_action(port_2, data)
+        write_com_action(port_1, data)
     elif string == "Key.application":
         data = b'x'
-        write_com_action(port_2, data)
+        write_com_action(port_1, data)
     elif string == "Key.control":
         data = b'c'
-        write_com_action(port_2, data)
+        write_com_action(port_1, data)
     elif string == "Key.shift":
         data = b'v'
-        write_com_action(port_2, data)
+        write_com_action(port_1, data)
     elif string == "Key.alt":
         data = b'b'
-        write_com_action(port_2, data)
+        write_com_action(port_1, data)
     elif string == "Key.right":
         data = b'\x1b[C'
-        write_com_action(port_2, data)
+        write_com_action(port_1, data)
     elif string == "Key.left":
         data = b'\x1b[D'
-        write_com_action(port_2, data)
+        write_com_action(port_1, data)
     elif string == "Key.up":
         data = b'\x1b[A'
-        write_com_action(port_2, data)
+        write_com_action(port_1, data)
     elif string == "Key.down":
         data = b'\x1b[B'
-        write_com_action(port_2, data)
+        write_com_action(port_1, data)
+    elif string == "Key.enter":
+        data = '\r'
+        write_button(data)
+    elif string == "Key.escape":
+        data = '\x1b'
+        write_button(data)
+    elif string == "Key.backspace":
+        data = '\x7f'
+        write_button(data)
+    elif string == "Key.tab":
+        data = '\t'
+        write_button(data)
+    elif string == "Key.space":
+        data = ' '
+        write_button(data)  
     else:
         pass
 
 def write_button(string):
     data = string
-    write_com_action(port_1, data)
+    data = string.encode()
+    write_com_action(port_2, data)
---
+++ server_util/server_main.py
@@ -0,0 +1,51 @@
+from  command_mcu import replase_server
+#from keyboard_emulator import read_keyboard
+import time
+
+def save_number_server(input):
+    with open('number_server.txt', 'w+') as f:
+        f.write(input)
+        f.write("\n")
+    f.close()
+
+
+def read_number_server_earlier():
+    with open('number_server.txt', 'r') as f:
+        lines = f.readline()
+        lines = lines.replace(' ','')
+        lines = lines.replace('\n','')
+        lines = lines.replace('\r','')
+    f.close()
+    return lines
+
+
+while True:
+    try:
+        print("Выберите сервер для работы")
+        print("Для этого введите команду 'display_x где x номер сервера'")
+        print("Для выхода из режима нажмите клавишу home")
+        main_command = input()
+        main_command_less = main_command[0:8]
+        main_command_number = main_command[8:9]
+        main_command_number = int(main_command_number)
+        if main_command_number < 5:
+            main_command_number = str(main_command_number)
+            if len(main_command_number) == 0:
+                print("Введите команду заново")
+                break
+            else:
+                pass
+            if main_command_less == "display_":
+                print("Выбран сервер =",main_command_number)
+                val = read_number_server_earlier()
+                replase_server(main_command_number, val)
+                save_number_server(main_command_number)
+                time.sleep(1)
+                
+                break
+            else:
+                print("Такой команды нет")
+        else:
+            print("Неверный номер сервера!")
+    except KeyboardInterrupt:
+        break
